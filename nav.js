75f66b7482d0c9f6bc02ff99a231f605af6fb832
8BitRobot <ninjastrike454@gmail.com>
2020-03-15 07:11:26 -0700

    made the other pages responsive and broke the desktop site in the process

--- nav.js
+++ nav.js
@@ -1,42 +1,15 @@
-window.onload = function () {
-    const nav = document.querySelector('#nav');
-    const menu = document.querySelector('#menu');
-    const menuToggle = document.querySelector('.nav__toggle');
-    let isMenuOpen = false;
-
-
-    // TOGGLE MENU ACTIVE STATE
-    menuToggle.addEventListener('click', e => {
-        e.preventDefault();
-        isMenuOpen = !isMenuOpen;
-
-        // toggle a11y attributes and active class
-        menuToggle.setAttribute('aria-expanded', String(isMenuOpen));
-        menu.hidden = !isMenuOpen;
-        nav.classList.toggle('nav--open');
+$(document).ready(function() {
+    $("#mobile-nav-btn").click(function() {
+        $(".mobile-menu").fadeToggle();
+        document.body.style.top = `-${window.scrollY}px`;
+        document.body.style.position = 'fixed';
     });
 
-
-    // TRAP TAB INSIDE NAV WHEN OPEN
-    nav.addEventListener('keydown', e => {
-        // abort if menu isn't open or modifier keys are pressed
-        if (!isMenuOpen || e.ctrlKey || e.metaKey || e.altKey) {
-            return;
-        }
-
-        // listen for tab press and move focus
-        // if we're on either end of the navigation
-        const menuLinks = menu.querySelectorAll('.nav__link');
-        if (e.keyCode === 9) {
-            if (e.shiftKey) {
-                if (document.activeElement === menuLinks[0]) {
-                    menuToggle.focus();
-                    e.preventDefault();
-                }
-            } else if (document.activeElement === menuToggle) {
-                menuLinks[0].focus();
-                e.preventDefault();
-            }
-        }
+    $("#mobile-menu-close").click(function() {
+        $(".mobile-menu").fadeToggle();
+        const scrollY = document.body.style.top;
+        $("body").css("position", "");
+        $("body").css("top", "");
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
     });
-}
+});
